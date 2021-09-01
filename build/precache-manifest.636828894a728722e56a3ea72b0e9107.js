3d20209c32c0db622dd339027a07760fa23580a4
Naazim Jalal <61738508+naazimjalal@users.noreply.github.com>
2021-09-01 22:29:19 +0530

    Add files via upload

---
+++ build/precache-manifest.636828894a728722e56a3ea72b0e9107.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d207c3f18f5f08ccf39f32a3be52c527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a9b7ddae39cf3d958a7",
+    "url": "/static/css/main.92e33356.chunk.css"
+  },
+  {
+    "revision": "ed5d94f3ea929e5d64c7",
+    "url": "/static/js/2.4634752e.chunk.js"
+  },
+  {
+    "revision": "cc2beb7fd0c7c34c0396",
+    "url": "/static/js/3.1a8291b1.chunk.js"
+  },
+  {
+    "revision": "0a9b7ddae39cf3d958a7",
+    "url": "/static/js/main.41d90c78.chunk.js"
+  },
+  {
+    "revision": "ae60d6630b4e5e88340d",
+    "url": "/static/js/runtime~main.17f7a0d1.js"
+  },
+  {
+    "revision": "39a2c7f346d5cfae7045aeb2fb50d9ad",
+    "url": "/static/media/Lulo.39a2c7f3.ttf"
+  },
+  {
+    "revision": "03f5a41dbce213143977eb08838dc229",
+    "url": "/static/media/Med.03f5a41d.jpg"
+  },
+  {
+    "revision": "f9b597c08458f4e9fb7e52b7fec39fd3",
+    "url": "/static/media/Platform.f9b597c0.ttf"
+  },
+  {
+    "revision": "4e8910add3f5f265b446f80147a73973",
+    "url": "/static/media/UniSans.4e8910ad.otf"
+  },
+  {
+    "revision": "bdabff3aad0556b2a6cca17435bc7371",
+    "url": "/static/media/Untitled.bdabff3a.jpg"
+  },
+  {
+    "revision": "84160ba75bd6f1d03e5843243f02aa06",
+    "url": "/static/media/bird.84160ba7.jpg"
+  },
+  {
+    "revision": "d9e0aca167a893e1782f8e5bac5267a6",
+    "url": "/static/media/cat.d9e0aca1.jpg"
+  },
+  {
+    "revision": "34eb2de7fee222b16869b4a35655b11f",
+    "url": "/static/media/contact.34eb2de7.jpg"
+  },
+  {
+    "revision": "a05795c351485606034a0782f2c18334",
+    "url": "/static/media/custom.a05795c3.jpg"
+  },
+  {
+    "revision": "aea244c1294d0a9f23aac81be18ceb95",
+    "url": "/static/media/dog.aea244c1.jpg"
+  },
+  {
+    "revision": "71c35d0b89e0e1eb1964330b0f08bcdb",
+    "url": "/static/media/fish.71c35d0b.jpg"
+  },
+  {
+    "revision": "6dec577727f4c9fd7d59b04cb80d7f43",
+    "url": "/static/media/fishin.6dec5777.jpg"
+  },
+  {
+    "revision": "8f975acf2fb4203e8c21d1fda7452c31",
+    "url": "/static/media/fishtank.8f975acf.jpg"
+  },
+  {
+    "revision": "842d4ebac9eea3bde21b64c314d3ac15",
+    "url": "/static/media/logo-new.842d4eba.jpg"
+  },
+  {
+    "revision": "2512aef7e23a3d89acfb89bdf1c07773",
+    "url": "/static/media/logo.2512aef7.jpg"
+  },
+  {
+    "revision": "5e55b77433c7a4a31bbfbe0446a0087e",
+    "url": "/static/media/map.5e55b774.png"
+  },
+  {
+    "revision": "63617886fbde2dcc46c4fe06661a8acb",
+    "url": "/static/media/proxima.63617886.otf"
+  },
+  {
+    "revision": "b7a9308e644d3818ef837534b20a2b92",
+    "url": "/static/media/slide1.b7a9308e.png"
+  },
+  {
+    "revision": "a152e4af7dcd90df0b40de0f932a0c4b",
+    "url": "/static/media/slide2.a152e4af.jpg"
+  },
+  {
+    "revision": "4026e0961a7bb1ab7a4276ea76a0b28c",
+    "url": "/static/media/slide3.4026e096.png"
+  },
+  {
+    "revision": "2158702bc44c3411096c764ef1765e9a",
+    "url": "/static/media/slide4.2158702b.jpg"
+  },
+  {
+    "revision": "ff765992e7c14619279bd7b5ca4501cb",
+    "url": "/static/media/slide5.ff765992.png"
+  },
+  {
+    "revision": "6da44a255b58e41ae070790b16d00ba2",
+    "url": "/static/media/slide6.6da44a25.jpg"
+  },
+  {
+    "revision": "b2dcc3da4436669c9beb7e0a974be27a",
+    "url": "/static/media/slide7.b2dcc3da.png"
+  }
+]);
